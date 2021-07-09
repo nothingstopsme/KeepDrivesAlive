d84871850f7bc274b52c05ee89da8a818cdb7a39
nothingstopsme <nothingstopsme@github.com>
2021-07-09 22:28:46 +0800

    renewing the assembly name accidentally brought over from a previous project

--- KeepDrivesAlive/Properties/AssemblyInfo.cs
+++ KeepDrivesAlive/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("LaunchToBackground")]
+[assembly: AssemblyTitle("KeepDrivesAlive")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("LaunchToBackground")]
+[assembly: AssemblyProduct("KeepDrivesAlive")]
 [assembly: AssemblyCopyright("Copyright ©  2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
